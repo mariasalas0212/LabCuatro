e3ec29f7b7b812ce4b2fb2b168e35dd9a9b8838b
mariasalas0212 <mariajsalas0212@gmail.com>
2026-03-26 11:34:16 -0600

    se terminó el javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+function cambioRojo(){
+    document.body.style.backgroundColor="red";
+}
+function cambioAzul(){
+    document.body.style.backgroundColor="blue";
+}
+
+function cambioVerde(){
+    document.body.style.backgroundColor="green";
+}
